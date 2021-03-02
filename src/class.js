79250ef93363f3cb8fdde8419de7df48af0c4c90
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-02 12:48:29 +0900

    classの継承の記述方法

--- src/class.js
+++ src/class.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Person = /** @class */ (function () {
     function Person(name, age) {
         this.name = name;
@@ -11,3 +24,19 @@ var Person = /** @class */ (function () {
 }());
 var hoge = new Person("hoge", 20);
 hoge.greeting();
+var Teacher = /** @class */ (function (_super) {
+    __extends(Teacher, _super);
+    // もし初期化時点でプロパティを追加したいなら以下を書く必要がある
+    function Teacher(name, age, subject) {
+        var _this = _super.call(this, name, age) || this;
+        _this.subject = subject;
+        return _this;
+    }
+    // メソッドの上書きがしたい場合は、書くしかない。
+    Teacher.prototype.greeting = function () {
+        console.log("I'm " + this.name + ",I am " + this.age + " old, I teach " + this.subject + ".");
+    };
+    return Teacher;
+}(Person));
+var teacher = new Teacher("sensei", 40, "English");
+teacher.greeting();
--- src/class.ts
+++ src/class.ts
@@ -1,5 +1,5 @@
 class Person {
-  constructor(public name:string,private age:number) {
+  constructor(public name:string,protected age:number) {
   }
   greeting() {
     console.log(`I'm ${this.name},I am ${this.age} old`)
@@ -9,3 +9,16 @@ class Person {
 const hoge = new Person("hoge",20);
 hoge.greeting()
 
+class Teacher extends Person{
+  // もし初期化時点でプロパティを追加したいなら以下を書く必要がある
+  constructor(name:string,age:number,public subject:string) {
+    super(name,age)
+  }
+  // メソッドの上書きがしたい場合は、書くしかない。
+  greeting() {
+    console.log(`I'm ${this.name},I am ${this.age} old, I teach ${this.subject}.`)
+  }
+
+}
+const teacher = new Teacher("sensei", 40,"English")
+teacher.greeting()
