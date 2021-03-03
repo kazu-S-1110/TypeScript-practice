0031d91a79e382698e78dcde4a85193ae17dfd3b
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-03 12:43:26 +0900

    abstractを学習

--- src/class.js
+++ src/class.js
@@ -22,62 +22,33 @@ var Person = /** @class */ (function () {
             return true;
         return false;
     };
-    //もしstaticの値にアクセスする場合はthisを使わずにクラス名を書く
     Person.prototype.incrementAge = function () {
         this.age += 1;
-        Person.species = "changed!";
     };
     Person.prototype.greeting = function () {
         console.log("I'm " + this.name + ",I am " + this.age + " old");
+        //継承先のメソッドにアクセスしたい場合はabstractを使用すること。使用する場合にはクラスの宣言時にも書くこと
+        this.explainJob();
     };
-    //インスタンスを作らずにクラスの値を使う方法（newとか使わない方法）
     Person.species = "ホモ・サピエンス";
     return Person;
 }());
-// const hoge = new Person("hoge",20);
-// hoge.greeting()
+//abstractを使用した場合、そのクラスからインスタンスを作成することは不可能。継承前提で書く。
+//失敗例　const hoge = new Person
 var Teacher = /** @class */ (function (_super) {
     __extends(Teacher, _super);
-    // もし初期化時点でプロパティを追加したいなら以下を書く必要がある
     function Teacher(name, age, _subject) {
         var _this = _super.call(this, name, age) || this;
         _this._subject = _subject;
+        _this.greeting();
         return _this;
     }
-    Object.defineProperty(Teacher.prototype, "subject", {
-        // ゲッターはなにかを取得したときに走る関数のこと、セッターはなにかを変更したときに走る関数のこと（ES3では扱えないので注意）
-        //ゲッターは必ず一つ以上の値を返すようにする
-        get: function () {
-            if (!this._subject) {
-                throw new Error('There is no subject');
-            }
-            return this._subject;
-        },
-        //セッターは必ず一つ以上のプロパティを含める、またゲッターと同じ関数名を設定することが可能(ただし、扱う型は同一でなければならない）
-        set: function (value) {
-            if (!value) {
-                throw new Error('There is no subject');
-            }
-            else {
-                this._subject = value;
-            }
-        },
-        enumerable: false,
-        configurable: true
-    });
-    // メソッドの上書きがしたい場合は、書くしかない。
+    Teacher.prototype.explainJob = function () {
+        console.log("I'm teacher and I teach " + this._subject);
+    };
     Teacher.prototype.greeting = function () {
-        console.log("I'm " + this.name + ",I am " + this.age + " old, I teach " + this.subject + ".");
+        console.log("I'm " + this.name + ",I am " + this.age + " old, I teach " + this._subject + ".");
     };
     return Teacher;
 }(Person));
 var teacher = new Teacher("sensei", 40, "English");
-// teacher._subject = "Math"
-// teacher.greeting()
-console.log(Person.species);
-console.log(Person.isAdult(20));
-//継承先でもstaticにしたものは呼び出しが可能
-console.log(Teacher.species);
-console.log(Teacher.isAdult(30));
-teacher.incrementAge();
-console.log(Person.species);
--- src/class.ts
+++ src/class.ts
@@ -1,5 +1,4 @@
-class Person {
-  //インスタンスを作らずにクラスの値を使う方法（newとか使わない方法）
+abstract class Person {
   static species = "ホモ・サピエンス"
   static isAdult(age: number) {
     if (age > 17) return true
@@ -7,57 +6,32 @@ class Person {
   }
   constructor(public name:string,protected age:number) {
   }
-  //もしstaticの値にアクセスする場合はthisを使わずにクラス名を書く
   incrementAge() {
     this.age += 1
-    Person.species = "changed!"
   }
 
   greeting() {
     console.log(`I'm ${this.name},I am ${this.age} old`)
+    //継承先のメソッドにアクセスしたい場合はabstractを使用すること。使用する場合にはクラスの宣言時にも書くこと
+    this.explainJob()
   }
+  abstract explainJob(): void 
 }
+//abstractを使用した場合、そのクラスからインスタンスを作成することは不可能。継承前提で書く。
+//失敗例　const hoge = new Person
 
-// const hoge = new Person("hoge",20);
-// hoge.greeting()
 
 class Teacher extends Person{
-  // ゲッターはなにかを取得したときに走る関数のこと、セッターはなにかを変更したときに走る関数のこと（ES3では扱えないので注意）
-  //ゲッターは必ず一つ以上の値を返すようにする
-  get subject():string {
-    if (!this._subject) {
-      throw new Error('There is no subject')
-    }
-    return this._subject
-  }
-  //セッターは必ず一つ以上のプロパティを含める、またゲッターと同じ関数名を設定することが可能(ただし、扱う型は同一でなければならない）
-  set subject(value) {
-    if (!value) {
-      throw new Error('There is no subject')
-    } else {
-      this._subject = value
-    }
-  }
-
-  // もし初期化時点でプロパティを追加したいなら以下を書く必要がある
   constructor(name:string,age:number,public _subject:string) {
-    super(name,age)
+    super(name, age)
+    this.greeting()
   }
-  // メソッドの上書きがしたい場合は、書くしかない。
+  explainJob() {
+    console.log(`I'm teacher and I teach ${this._subject}`)
+  }
   greeting() {
-    console.log(`I'm ${this.name},I am ${this.age} old, I teach ${this.subject}.`)
+    console.log(`I'm ${this.name},I am ${this.age} old, I teach ${this._subject}.`)
   }
 
 }
 const teacher = new Teacher("sensei", 40, "English")
-// teacher._subject = "Math"
-// teacher.greeting()
-
-console.log(Person.species)
-console.log(Person.isAdult(20))
-//継承先でもstaticにしたものは呼び出しが可能
-console.log(Teacher.species)
-console.log(Teacher.isAdult(30))
-
-teacher.incrementAge()
-console.log(Person.species)
