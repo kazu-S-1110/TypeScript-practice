af687f32dfe01ea05e78ca368aab2761bb99596b
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-07 20:07:28 +0900

    タイプガード３選

--- src/advanced.js
+++ src/advanced.js
@@ -1,7 +1,52 @@
 "use strict";
-// interface EnfineerBlogger extends Bngineer, Blogger {} //この記述も可能
 var jack = {
     name: "Jack",
     role: "Back-end",
     follower: 300
 };
+function toUpperCase(x) {
+    //①typeofを使用して分岐させる
+    if (typeof x === "string") {
+        return x.toUpperCase();
+    }
+    return "";
+}
+function describeProfile(nomadworker) {
+    console.log(nomadworker.name); //nameしかアクセスできない、EngineerとBloggerに共通してるプロパティのみ
+    // ②もし片方しかないオブジェクトのキーにアクセスしたい場合in演算子を使う
+    if ("role" in nomadworker) {
+        console.log(nomadworker.role);
+    }
+    if ("follower" in nomadworker) {
+        console.log(nomadworker.follower);
+    }
+}
+var Dog = /** @class */ (function () {
+    function Dog() {
+    }
+    Dog.prototype.speak = function () {
+        console.log("Bow-wow");
+    };
+    return Dog;
+}());
+var Bird = /** @class */ (function () {
+    function Bird() {
+    }
+    Bird.prototype.speak = function () {
+        console.log("tweet-tweet");
+    };
+    Bird.prototype.fly = function () {
+        console.log("flutter");
+    };
+    return Bird;
+}());
+function havePet(pet) {
+    pet.speak(); //speakはどちらにもあるのでアクセスが可能
+    // ③キーではなくインスタンスオブを使用してタイプガードを行う
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+// havePet(new Dog())
+havePet(new Bird()); //Birdから生成されたインスタンスのみflyが行われる
+havePet({ speak: function () { console.log("Heloo"); }, fly: function () { console.log("not fly"); } });
--- src/advanced.ts
+++ src/advanced.ts
@@ -1,4 +1,4 @@
-//インターセクション型
+// 条件文を使って型を絞り込む記述3つ(タイプガードと呼ばれる)　
 type Engineer = {
   name: string
   role:string
@@ -7,16 +7,56 @@ type Blogger = {
   name: string
   follower:number
 }
-// ＆マークでくっつける (くっつける対象がtypeでもinterfaceでも可能　)
 type EngineerBlogger = Engineer & Blogger
-// interface EnfineerBlogger extends Bngineer, Blogger {} //この記述も可能
 
-const jack = {
+const jack:EngineerBlogger= {
   name: "Jack",
   role: "Back-end",
   follower:300
 }
 
-type NumberBoolean = number | boolean
-type StringNumber = string | number
-type Mix = NumberBoolean & StringNumber　//ユニオン型をインターセクションにする場合はベン図のように考える
+function toUpperCase(x: string | number) {
+  //①typeofを使用して分岐させる
+  if (typeof x === "string") {
+    return x.toUpperCase() 
+  }
+  return ""
+}
+
+type NomadWorker = Engineer | Blogger 
+function describeProfile(nomadworker:NomadWorker) {
+  console.log(nomadworker.name) //nameしかアクセスできない、EngineerとBloggerに共通してるプロパティのみ
+  // ②もし片方しかないオブジェクトのキーにアクセスしたい場合in演算子を使う
+  if ("role" in nomadworker) {
+    console.log(nomadworker.role)
+  }
+  if ("follower" in nomadworker) {
+    console.log(nomadworker.follower)
+  }
+}
+
+class Dog {
+  speak() {
+    console.log("Bow-wow")
+  }
+} 
+class Bird {
+  speak() {
+    console.log("tweet-tweet")
+  }
+  fly() {
+    console.log("flutter")
+  } 
+} 
+type Pet = Dog | Bird
+function havePet(pet:Pet) {
+  pet.speak() //speakはどちらにもあるのでアクセスが可能
+  // ③キーではなくインスタンスオブを使用してタイプガードを行う
+  if (pet instanceof Bird) {
+    pet.fly()
+  }
+}
+
+// havePet(new Dog())
+havePet(new Bird())//Birdから生成されたインスタンスのみflyが行われる
+havePet({speak(){console.log("Hello")} ,fly(){console.log("not fly")}})//helloは出力されるがflyは無効
