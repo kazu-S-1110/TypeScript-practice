83b4e13886c2c925e06fcb67df2a563ccb50e20e
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-16 08:30:49 +0900

    keyofを使用してオブジェクトキーのユニオン型を作る方法を学習

--- src/generics.js
+++ src/generics.js
@@ -1,7 +1,8 @@
 "use strict";
-//extendsを使用して型パラメータに制約をつける
-//型パラメータに最低限実装した型を指定するために用いる
-function copy(value) {
+//keyofを使用してオブジェクトキーのユニオン型を作る方法
+//動的に対応できるため、以下の書き方に慣れると良い。
+function copy(value, key) {
+    console.log(value[key]);
     return value;
 }
-console.log(copy({ name: 'Jack' }));
+console.log(copy({ name: 'Jack', age: 28, height: 170 }, "height")); //オブジェクトキーが複数でも対応ができる
--- src/generics.ts
+++ src/generics.ts
@@ -1,7 +1,15 @@
-//extendsを使用して型パラメータに制約をつける
+//keyofを使用してオブジェクトキーのユニオン型を作る方法
 
-//型パラメータに最低限実装した型を指定するために用いる
-function copy<T extends {name: string}>(value: T): T { //これによりTは最低限name:stringを持ったものが保証される
+type K = keyof { //keyofを使用することでキーのみを取得する、複数取得するとユニオン型となる。
+  name: string
+  age:number
+}// ジェネリクスと組み合わせることが多い
+
+//動的に対応できるため、以下の書き方に慣れると良い。
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T { 
+  console.log(value[key]) 
   return value;
 }
-console.log(copy({ name: 'Jack' }));
+console.log(copy({ name: 'Jack', age: 28, height: 170 }, "height"));//オブジェクトキーが複数でも対応ができる
+
+
