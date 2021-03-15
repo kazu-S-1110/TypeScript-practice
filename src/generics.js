4a54fad908c117f0ef0d59ed8abe9d9a1f82e1fb
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-16 08:12:23 +0900

    extendsを使用して型パラメータに制約をつける方法を学習

--- src/generics.js
+++ src/generics.js
@@ -1,13 +1,7 @@
-//ジェネリクスを使用して型を引数として扱う
+"use strict";
+//extendsを使用して型パラメータに制約をつける
+//型パラメータに最低限実装した型を指定するために用いる
 function copy(value) {
     return value;
 }
-console.log(copy('hello')); //こう書いてしまうとcopyから先の関数を使えない、toUppercaseとか使えない（回避策としてasを使うとか）
-// そこで役立つのがジェネクリス
-function copy2(value) {
-    //書き方は角括弧を関数名の直後に書く
-    return value;
-}
-console.log(copy2('hello').toUpperCase());
-// また型推論も効く
-console.log(copy2({ name: 'Jack' }.name));
+console.log(copy({ name: 'Jack' }));
--- src/generics.ts
+++ src/generics.ts
@@ -1,14 +1,7 @@
-//ジェネリクスを使用して型を引数として扱う
-function copy(value: any) {
-  return value;
-}
-console.log(copy('hello')); //こう書いてしまうとcopyから先の関数を使えない、toUppercaseとか使えない（回避策としてasを使うとか）
+//extendsを使用して型パラメータに制約をつける
 
-// そこで役立つのがジェネクリス
-function copy2<T>(value: T): T {
-  //書き方は角括弧を関数名の直後に書く
+//型パラメータに最低限実装した型を指定するために用いる
+function copy<T extends {name: string}>(value: T): T { //これによりTは最低限name:stringを持ったものが保証される
   return value;
 }
-console.log(copy2<string>('hello').toUpperCase());
-// また型推論も効く
-console.log(copy2({ name: 'Jack' }.name));
+console.log(copy({ name: 'Jack' }));
