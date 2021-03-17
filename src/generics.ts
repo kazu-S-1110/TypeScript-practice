e0c6a15b47320c6e5815059182734ddb51bb1e45
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-17 12:41:29 +0900

    MappedTypesを学習（型のfor文的な感じ）

--- src/generics.ts
+++ src/generics.ts
@@ -1,24 +1,10 @@
-// デフォルトの型パラメータの書き方
-
-interface Todo {
-  title: string
-  text:string
+// MappedTypesを学習（型のfor文的な感じ）
+interface Tmp {
+  readonly "tomato": string
+  "pumpkin":string
 }
-type Todoable = Partial<Todo> //全てオプショナルにする
-type ReadTodo = Readonly<Todo>　//全てReadOnlyにする
 
-const fetchData :Promise<string>= new Promise(resolve => {
-  setTimeout(() => {
-    resolve("hello")
-  }, 3000);
-})
-
-fetchData.then(data => {
-  data.toUpperCase()
-})
-
-interface ResponseData<T = string> { //イコールで繋げると初期値が設定できる。もし書いてないと設定する際に引数が必要になる。
-  data: T
-  status: number
+type MappedTypes= {
+  -readonly [P in keyof Tmp]-?: P //こう書くとリテラル型になる,stringにすると文字列型になる、また先頭にreadonlyをつけることも可能, -（マイナス）をつけるとreadonlyを消すことができる
 }
-let tmp: ResponseData
+
