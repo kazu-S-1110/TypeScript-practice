ced52db33fa8fe72ab68593136a769bb601609b2
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-18 08:30:41 +0900

    conditionaltypes (型のif)を学習

--- src/generics.ts
+++ src/generics.ts
@@ -1,10 +1,23 @@
-// MappedTypesを学習（型のfor文的な感じ）
-interface Tmp {
-  readonly "tomato": string
-  "pumpkin":string
-}
+// conditional types (型のif)
 
-type MappedTypes= {
-  -readonly [P in keyof Tmp]-?: P //こう書くとリテラル型になる,stringにすると文字列型になる、また先頭にreadonlyをつけることも可能, -（マイナス）をつけるとreadonlyを消すことができる
-}
+type conditionalTypes = "tomato" extends string ? number : boolean; //三項演算子のような書き方
+type conditionalTypes2 = 33 extends string ? number : boolean;
+type conditionalTypes3 = string extends "tomato" ? number : boolean;
 
+//infer R の使い方
+type conditionalTypeInfer = { tomato: "tomato" } extends { tomato: infer R } ? R : boolean;
+type conditionalTypeInfer2 = { tomato: string } extends { tomato: infer R } ? R : boolean;
+type conditionalTypeInfer3 = { banana: "tomato" } extends { tomato: infer R } ? R : boolean;
+// 代入元にRを返す、もしInfer以外が一致しないときはfalseとなる
+// utility(ReturnType)がある 
+// type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+
+
+
+// DistributiveConditionalTypes の使い方
+type DistributiveConditionalTypes<T> = T extends "tomato" ? number : boolean;
+let tmp: DistributiveConditionalTypes<"tomato" | "pumpkin">;
+let tmp2: DistributiveConditionalTypes<"hoge" | "tomato">
+// ユニオン型になるようなもの
+// utility(NonNullable)がある
+let tmp3 :NonNullable<string | null> //type NonNullable<T> = T extends null | undefined ? never : T;
