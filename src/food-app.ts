5a8afdaf6ea89a68b8b1f9e1d63a32c4c4511219
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-23 06:21:43 +0900

    アプリ練習：activeクラスを配列に入れる処理

--- src/food-app.ts
+++ src/food-app.ts
@@ -9,6 +9,17 @@ class Food {
 }
 class Foods {
   elements = document.querySelectorAll<HTMLDivElement>('.food');
+  private _activeElements: HTMLDivElement[] = []; //food--activeのものを入れる配列を用意
+  get activeElements() {
+    this._activeElements = [];
+    this.elements.forEach((element) => {
+      if (element.classList.contains('food--active')) {
+        //food--activeのクラスを持つか判別するif文
+        this._activeElements.push(element);
+      }
+    });
+    return this._activeElements;
+  }
   constructor() {
     this.elements.forEach((element) => {
       new Food(element);
