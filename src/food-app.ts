e4793f631b5d90af8c6b009d8a0972d0ba39ae68
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-24 08:10:29 +0900

    アプリ練習：取得した配列から数字を抽出する

--- src/food-app.ts
+++ src/food-app.ts
@@ -10,6 +10,7 @@ class Food {
 class Foods {
   elements = document.querySelectorAll<HTMLDivElement>('.food');
   private _activeElements: HTMLDivElement[] = []; //food--activeのものを入れる配列を用意
+  private _activeElementsScore: number[] = [];  //activeElementsから数字を抽出するnumber型の配列を用意
   get activeElements() {
     this._activeElements = [];
     this.elements.forEach((element) => {
@@ -19,6 +20,16 @@ class Foods {
       }
     });
     return this._activeElements;
+  }
+  get activeElementsScore() {
+    this._activeElementsScore = []
+    this._activeElements.forEach(element => {
+      const foodScore = element.querySelector(".food__score")
+      if (foodScore) {
+        this._activeElementsScore.push(Number(foodScore.textContent)) //Scoreの配列はnumber型なのでnumber型に変換する
+      }
+    })
+    return this._activeElementsScore
   }
   constructor() {
     this.elements.forEach((element) => {
