b4cb062016d05b66282e1c0285ede01add843534
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-28 22:30:11 +0900

    webpackのconfigファイルを編集。bundle方法を学習。

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require('path');
+
 module.exports = {
   entry: './dist/main.js', //webpackがビルドする際に開始点となるJSファイルを設定
+  output: {
+    filename: 'bundle.js', //[contenthash]と先頭に書くことで中身をハッシュ化した値を付与できる。キャッシュによる齟齬の対策に有効。
+    path: path.resolve(__dirname, 'dist'), //pathには絶対パスを指定する。webpack.configはcommonjsのため、requireでpathを取得できる。resolveのメソッドで回すことで簡単に書くことが可能。windowsだとスラッシュで区切られてないので有用。
+  },
 };
