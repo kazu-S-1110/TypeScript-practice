269708c18a943d18de19ce31a4a4562a1d6c5a58
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-29 12:44:53 +0900

    webpackでsourceMapの表示

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,5 @@ module.exports = {
     filename: 'bundle.js', //[contenthash]と先頭に書くことで中身をハッシュ化した値を付与できる。キャッシュによる齟齬の対策に有効。
     path: path.resolve(__dirname, 'dist'), //pathには絶対パスを指定する。webpack.configはcommonjsのため、requireでpathを取得できる。resolveのメソッドで回すことで簡単に書くことが可能。windowsだとスラッシュで区切られてないので有用。
   },
+  devtool: 'inline-source-map', //ソースマップの表示。https://webpack.js.org/configuration/devtool/で詳しく見れるよ
 };
