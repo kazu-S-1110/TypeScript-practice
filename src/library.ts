c48279b76ff612d5af0ea765e43d92e8718ef3b6
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-03-31 21:36:21 +0900

    namespaceでの型の記述方法を学習

--- src/library.ts
+++ src/library.ts
@@ -9,5 +9,9 @@ console.log(_.shuffle([1, 2, 3, 4]))
 namespace myApp { //スコープを限定して使える、使いたいものがあればexportする。最近はあまり使われなくなってきた。
   const hello = "hello in namespace"
   export const name = "Jack"
+  export interface Nameable {
+    name:string
+  }
 }
-const hello = "yeah!"
+const hello = myApp.name
+let nameable: myApp.Nameable //型注釈でドットが使われている場合はnamespaceが使われていると気づくこと。
