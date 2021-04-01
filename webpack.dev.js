38af3cbc46aa08751af9c2bf5131c915456c835e
kazu-S-1110 <xtpgdmw3690@yahoo.co.jp>
2021-04-01 12:59:58 +0900

    reactを導入してみる

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'development', //バンドルファイルの形式を選択可能、デフォは"production"
   //使い分けることが一般的なのでdev-serverのときはdev、buildしたい時はproductionとする。
   // webpackのconfigファイルを２つに分ければ解決。package.jsonに追記する。
-  entry: '/src/library.ts', //webpackがビルドする際に開始点となるファイルを設定
+  entry: '/src/react.tsx', //webpackがビルドする際に開始点となるファイルを設定
   output: {
     filename: 'bundle.js', //[contenthash]と先頭に書くことで中身をハッシュ化した値を付与できる。キャッシュによる齟齬の対策に有効。
     path: path.resolve(__dirname, 'dist'), //pathには絶対パスを指定する。webpack.configはcommonjsのため、requireでpathを取得できる。resolveのメソッドで回すことで簡単に書くことが可能。windowsだとスラッシュで区切られてないので有用。
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -21,6 +21,6 @@ module.exports = {
   },
   resolve: {
     //import文の.jsや.tsの表記を無くすことが可能になる。
-    extensions: ['.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
 };
